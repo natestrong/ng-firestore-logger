d7e37509082551fd79d06dc4fa82e740283df03f
nstrong <nathanstrong.fx@gmail.com>
2021-08-10 06:28:59 -0700

    updated readme

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,4 @@
 export const environment = {
   production: true,
   useEmulators: false,
-  firebase: {
-    apiKey: "AIzaSyAwSNNvsSHJJL4U_2jnmlB-CSMhV38ecVc",
-    authDomain: "firestore-logger.firebaseapp.com",
-    projectId: "firestore-logger",
-    storageBucket: "firestore-logger.appspot.com",
-    messagingSenderId: "1025032333247",
-    appId: "1:1025032333247:web:042606bc41f38e6e49230a",
-    measurementId: "G-3EX8130N6D"
-  }
 };
